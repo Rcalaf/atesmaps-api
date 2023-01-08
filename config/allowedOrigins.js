58612dab0725aad3e355a1c5b4ffd49a852533e6
Roger Calaf <rcalaf.cot@gmail.com>
2023-01-08 23:55:12 +1100

    v4.1

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
     'https://atesmaps.org',
-    'https://test.atesmaps.org/',
-    'https://api.atesmaps.org',
-    'https://api-test.atesmaps.org/',
+    'https://test.atesmaps.org:80',
+    // 'https://api.atesmaps.org',
+    // 'https://api-test.atesmaps.org',
     'http://127.0.0.1:3500',
     'http://localhost:3500',
     'http://localhost:3000',
