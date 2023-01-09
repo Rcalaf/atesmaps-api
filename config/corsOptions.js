2e2cede28c8837f6d6fee697fc90e3765af9a5e7
Roger Calaf <rcalaf.cot@gmail.com>
2023-01-09 20:34:38 +1100

    revert Cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,24 +1,24 @@
-const allowedOrigins = require('./allowedOrigins');
-
-const corsOptions = {
-  
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    preflightContinue: false,
-    credentials: true,
-    maxAge:5,
-    optionsSuccessStatus: 204
-}
+// const allowedOrigins = require('./allowedOrigins');
 
 // const corsOptions = {
-//     origin: true,
-//     optionsSuccessStatus: 200
+  
+//     origin: (origin, callback) => {
+//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     preflightContinue: false,
+//     credentials: true,
+//     maxAge:5,
+//     optionsSuccessStatus: 204
 // }
+
+// // const corsOptions = {
+// //     origin: true,
+// //     optionsSuccessStatus: 200
+// // }
 
 
 module.exports = corsOptions;
