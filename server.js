89d4b3c02bec2fc99b099fabfe5ac1858c629580
Roger Calaf <rcalaf.cot@gmail.com>
2023-01-09 20:50:13 +1100

    revert Cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 // const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
