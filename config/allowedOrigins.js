e98917375d6f336c4c1076ca1d1aa36cf14d7e77
Nil Torrano <nil.torrano@basetis.com>
2023-01-23 10:09:06 +0100

    [REF] Add obs domain to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'https://atesmaps.org',
     'https://test.atesmaps.org',
     'https://experimental.atesmaps.org',
-    'http://localhost:5000'
+    'http://localhost:5000',
+    'https://obs.atesmaps.org'
 ];
 
 module.exports = allowedOrigins;
