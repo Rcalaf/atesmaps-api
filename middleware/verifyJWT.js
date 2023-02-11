2cc7ff62ed69d9f45458d48e78ed41596c0795cf
Roger Calaf <rcalaf.cot@gmail.com>
2023-02-11 20:26:07 +1100

    fixing JWT token issue

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -4,12 +4,14 @@ const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
     if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
     const token = authHeader.split(' ')[1];
-    //console.log(token)
+    console.log(token)
+    console.log(process.env.ACCESS_TOKEN_SECRET)
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
             if (err){
+                console.log('error occurred...')
                 console.log(err);
                 return res.sendStatus(403); //invalid token
             } 
