5c699deae25770933249f8ea64e5782d8c0ef892
Roger Calaf <rcalaf.cot@gmail.com>
2024-01-20 07:07:49 +1100

    version update

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,7 +62,7 @@ const showVersion = (req, res) => {
 }
 
 const showIosVersion = (req, res) => {
-    res.status(200).json({ 'version': '1.0.7' });
+    res.status(200).json({ 'version': '1.0.8' });
 }
 
 const showAndroidVersion = (req, res) => {
